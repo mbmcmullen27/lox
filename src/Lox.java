b77cb0710fedc60b64707f978d6116658b23b633
fishbot <mbmcmullen27@gmail.com>
2021-06-02 21:19:40 -0500

    Chapter 6 - parsing

--- src/Lox.java
+++ src/Lox.java
@@ -47,11 +47,13 @@ public class Lox {
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
 
-        //For now... print tokens
-        for (Token token: tokens) {
-            System.out.println(token);
-        }
+        // Stop if there was a syntax error.
+        if (hadError) return;
+
+        System.out.println(new AstPrinter().print(expression));
     }
 
     static void error(int line, String message) {
@@ -61,5 +63,13 @@ public class Lox {
     private static void report(int line, String where, String message) {
         System.out.println("[Line " + line + "] Error" + where + ": " + message);
         hadError = true;
+    }
+
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
     }
 }
