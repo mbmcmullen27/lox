3fe8021db91d7d811e50818eaf0dff177f136044
mbmcmullen27 <mbmcmullen27@gmail.com>
2021-06-11 23:44:26 -0500

    add var to AST

--- src/Expr.java
+++ src/Expr.java
@@ -3,30 +3,11 @@ import java.util.List;
 
 abstract class Expr{
 	interface Visitor<R> {
-		R visitTernaryExpr(Ternary expr);
 		R visitBinaryExpr(Binary expr);
 		R visitGroupingExpr(Grouping expr);
 		R visitLiteralExpr(Literal expr);
 		R visitUnaryExpr(Unary expr);
-	}
-	static class Ternary extends Expr {
-		Ternary(Expr conditional, Token operator, Expr pass, Token operator2, Expr fail) {
-			this.conditional = conditional;
-			this.operator = operator;
-			this.pass = pass;
-			this.operator2 = operator2;
-			this.fail = fail;
-		}
-		@Override
-		<R> R accept(Visitor<R> visitor) {
-			return visitor.visitTernaryExpr(this);
-		}
-
-		final Expr conditional;
-		final Token operator;
-		final Expr pass;
-		final Token operator2;
-		final Expr fail;
+		R visitVariableExpr(Variable expr);
 	}
 	static class Binary extends Expr {
 		Binary(Expr left, Token operator, Expr right) {
@@ -37,7 +18,7 @@ abstract class Expr{
 
 		@Override
 		<R> R accept(Visitor<R> visitor) {
-			return visitor.visitBinaryExpr(this);
+		return visitor.visitBinaryExpr(this);
 		}
 
 		final Expr left;
@@ -51,7 +32,7 @@ abstract class Expr{
 
 		@Override
 		<R> R accept(Visitor<R> visitor) {
-			return visitor.visitGroupingExpr(this);
+		return visitor.visitGroupingExpr(this);
 		}
 
 		final Expr expression;
@@ -63,7 +44,7 @@ abstract class Expr{
 
 		@Override
 		<R> R accept(Visitor<R> visitor) {
-			return visitor.visitLiteralExpr(this);
+		return visitor.visitLiteralExpr(this);
 		}
 
 		final Object value;
@@ -76,11 +57,23 @@ abstract class Expr{
 
 		@Override
 		<R> R accept(Visitor<R> visitor) {
-			return visitor.visitUnaryExpr(this);
+		return visitor.visitUnaryExpr(this);
 		}
 
 		final Token operator;
 		final Expr right;
+	}
+	static class Variable extends Expr {
+		Variable(Token name) {
+			this.name = name;
+		}
+
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+		return visitor.visitVariableExpr(this);
+		}
+
+		final Token name;
 	}
 
 	abstract <R> R accept(Visitor<R> visitor);
