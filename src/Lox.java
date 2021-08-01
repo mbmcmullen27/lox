447fc780feac1046f7b7e7e4d3807690a71527cc
fishbot <mbmcmullen27@gmail.com>
2021-08-01 10:06:00 -0500

    duplicate additions from the goofy merge

--- src/Lox.java
+++ src/Lox.java
@@ -62,12 +62,6 @@ public class Lox {
 
         // Stop if there was a resolution error.
         if (hadError) return;
-
-        Resolver resolver = new Resolver(interpreter);
-        resolver.resolve(statements);
-
-        // Stop if there was a resolution error.
-        if (hadError) return;
         
         interpreter.interpret(statements);
     }
