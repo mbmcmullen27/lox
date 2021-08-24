47dc431b50378e1ff56053ade07481359a8bb820
mbmcmullen27 <mbmcmullen27@gmail.com>
2021-08-24 07:41:45 -0500

    longConstant and lineStart

--- clox/chunk.h
+++ clox/chunk.h
@@ -6,20 +6,30 @@
 
 typedef enum {
     OP_CONSTANT,
+    OP_CONSTANT_LONG,
     OP_RETURN,
 } OpCode;
+
+typedef struct {
+    int offset;
+    int line;
+} LineStart;
 
 typedef struct {
     int count;
     int capacity;
     uint8_t* code;
-    int* lines;
     ValueArray constants;
+    int lineCount;
+    int lineCapacity;
+    LineStart* lines;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
 void writeChunk(Chunk* chunk, uint8_t byte, int line);
 int addConstant(Chunk* chunk, Value value);
+int writeConstant(Chunk* chunk, Value value, int line);
+int getLine(Chunk* chunk, int instruction);
 
 #endif
--- clox/debug.c
+++ clox/debug.c
@@ -18,6 +18,17 @@ static int constantInstruction(const char* name, Chunk* chunk, int offset) {
     return offset + 2;
 }
 
+static int longConstantInstruction(const char* name, Chunk* chunk, int offset) {
+    uint32_t constant = chunk->code[offset + 1] |
+                        (chunk->[offset + 2] << 8) |
+                        (chunk->[offset + 3] << 16)
+
+    printf("%-16s %4d '", name, constant);
+    printValue(chunk->constants.values[constant]);
+    printf("'\n");
+    return offset + 4;  
+}
+
 static int simpleInstruction(const char* name, int offset) {
     printf("%s\n", name);
     return offset + 1;
@@ -25,16 +36,19 @@ static int simpleInstruction(const char* name, int offset) {
 
 int disassembleInstruction(Chunk* chunk, int offset) {
     printf("%04d ", offset);
-    if (offset > 0 && chunk->lines[offset] == chunk->lines[offset - 1]) {
+    int line = getLine(chunk, offset);
+    if (offset > 0 && line == getLine(chunk, offset - 1)) {
         printf("   | ");
     } else {
-        printf("%4d ", chunk->lines[offset]);
+        printf("%4d ", line);
     }
 
     uint8_t instruction = chunk->code[offset];
     switch (instruction) {
         case OP_CONSTANT:
             return constantInstruction("OP_CONSTANT", chunk, offset);
+        case OP_CONSTANT_LONG:
+            return longConstantInstruction("OP_CONSTANT_LONG", chunk, offset);
         case OP_RETURN:
             return simpleInstruction("OP_RETURN", offset);
         default:
