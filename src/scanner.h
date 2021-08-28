a255ef66ea30668d23155c6f6f8c3a4970d862ba
mbmcmullen27 <mbmcmullen27@gmail.com>
2021-08-28 09:06:09 -0500

    start chapter 16

---
+++ src/scanner.h
@@ -0,0 +1,14 @@
+#ifndef clox_scanner_h
+#define clox_scanner_h
+
+typedef struct {
+    TokenType type;
+    const char* start;
+    int length;
+    int line;
+} Token;
+
+void initScanner(const char* source);
+Token scanToken();
+
+#endif
