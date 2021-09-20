5a2ea90744f9d465a835f084cafed83f502788e6
mbmcmullen27 <mbmcmullen27@gmail.com>
2021-09-20 08:04:57 -0500

    19.3-19.4

--- src/object.h
+++ src/object.h
@@ -25,7 +25,9 @@ struct ObjString {
     char* chars;
 };
 
+ObjString* takeString(char* chars, int length); 
 ObjString* copyString(const char* chars, int length);
+void printObject(Value value);
 
 static inline bool isObjType(Value value, ObjType type) {
     return IS_OBJ(value) && AS_OBJ(value)->type == type;
