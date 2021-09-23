7ff33c723b4d54a53b199c784fafca5edf240ee7
mbmcmullen27 <mbmcmullen27@gmail.com>
2021-09-23 07:35:28 -0500

    flexible array members for string instead of char* and malloc

--- src/memory.c
+++ src/memory.c
@@ -18,8 +18,7 @@ static void freeObject(Obj* object) {
     switch (object->type) {
         case OBJ_STRING: {
             ObjString* string = (ObjString*)object;
-            FREE_ARRAY(char, string->chars, string->length + 1);
-            FREE(ObjString, object);
+            reallocate(object, sizeof(ObjString) + string->length + 1, 0);
             break;
         }
     }
