c68b1b8ea8e7498c631c5c91f4330911a595e9a3
mbmcmullen27 <mbmcmullen27@gmail.com>
2021-09-26 14:25:16 -0500

    finish chapter 20

--- src/object.c
+++ src/object.c
@@ -2,6 +2,7 @@
 #include <string.h>
 
 #include "memory.h"
+#include "table.h"
 #include "object.h"
 #include "value.h"
 #include "vm.h"
@@ -23,12 +24,13 @@ static ObjString* allocateString(char* chars, int length, uint32_t hash) {
     string->length = length;
     string->chars = chars;
     string->hash = hash;
+    tableSet(&vm.strings, string, NIL_VAL);
     return string;
 }
 
 static uint32_t hashString(const char* key, int length) {
     uint32_t hash = 2166136261u;
-    for (int i - 0; i < length; i++) {
+    for (int i = 0; i < length; i++) {
         hash ^= (uint8_t)key[i];
         hash *= 16777619;
     }
@@ -36,13 +38,24 @@ static uint32_t hashString(const char* key, int length) {
 }
 
 ObjString* takeString(char* chars, int length) {
-    rint32_t hash = hashString(chars, length);
+    uint32_t hash = hashString(chars, length);
+    ObjString* interned = tableFindString(&vm.strings, chars, length, hash);
+
+    if(interned != NULL) {
+        FREE_ARRAY(char, chars, length+1);
+        return interned;
+    }
+
     return allocateString(chars, length, hash);
 }
 
 
 ObjString* copyString(const char* chars, int length) {
     uint32_t hash = hashString(chars, length);
+    ObjString* interned = tableFindString(&vm.strings, chars, length, hash);
+    
+    if (interned != NULL) return interned;
+
     char* heapChars = ALLOCATE(char, length + 1);
     memcpy(heapChars, chars, length);
     heapChars[length] = '\0';
--- src/table.h
+++ src/table.h
@@ -11,12 +11,16 @@ typedef struct {
 
 typedef struct {
     int count;
-    int caacity;
+    int capacity;
     Entry* entries;
 } Table;
 
 void initTable(Table* table);
 void freeTable(Table* table);
+bool tableGet(Table* table, ObjString* key, Value* value);
 bool tableSet(Table* table, ObjString* key, Value value);
+bool tableDelete(Table* table, ObjString* key);
+void tableAddAll(Table* from , Table* to);
+ObjString* tableFindString(Table* table, const char* chars, int length, uint32_t hash);
 
 #endif
