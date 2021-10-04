dea808c3c1b2a7290c7546f984650daed21223a8
mbmcmullen27 <mbmcmullen27@gmail.com>
2021-10-04 07:59:28 -0500

    read global vars

--- src/compiler.c
+++ src/compiler.c
@@ -210,6 +210,15 @@ static void string() {
     emitConstant(OBJ_VAL(copyString(parser.previous.start + 1, parser.previous.length -2)));
 }
 
+static void namedVariable(Token name) {
+    uint8_t arg = identifierConstant(&name);
+    emitBytes(OP_GET_GLOBAL, arg);
+}
+
+static void variable() {
+    namedVariable(parser.previous);
+}
+
 static void unary() {
     TokenType operatorType = parser.previous.type;
 
@@ -245,7 +254,7 @@ ParseRule rules[] = {
     [TOKEN_GREATER_EQUAL]   = {NULL,     binary,        PREC_COMPARISON},
     [TOKEN_LESS]            = {NULL,     binary,        PREC_COMPARISON},
     [TOKEN_LESS_EQUAL]      = {NULL,     binary,        PREC_COMPARISON},
-    [TOKEN_IDENTIFIER]      = {NULL,     NULL,          PREC_NONE},
+    [TOKEN_IDENTIFIER]      = {variable, NULL,          PREC_NONE},
     [TOKEN_STRING]          = {string,   NULL,          PREC_NONE},
     [TOKEN_NUMBER]          = {number,   NULL,          PREC_NONE},
     [TOKEN_AND]             = {NULL,     NULL,          PREC_NONE},
