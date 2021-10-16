05b38d51d324185be5c7003dec9bd0e1098447bc
mbmcmullen27 <mbmcmullen27@gmail.com>
2021-10-16 10:14:48 -0500

    23.2 - logical operators

--- src/compiler.c
+++ src/compiler.c
@@ -269,6 +269,15 @@ static void defineVariable(uint8_t global) {
     emitBytes(OP_DEFINE_GLOBAL, global);
 }
 
+static void and_(bool canAssign) {
+    int endJump = emitJump(OP_JUMP_IF_FALSE);
+
+    emitByte(OP_POP);
+    parsePrecedence(PREC_AND);
+
+    patchJump(endJump);
+}
+
 static void conditional() {
     // Compile the then branch.
     parsePrecedence(PREC_CONDITIONAL);
@@ -316,6 +325,17 @@ static void grouping(bool canAssign) {
 static void number(bool canAssign) {
     double value = strtod(parser.previous.start, NULL);
     emitConstant(NUMBER_VAL(value));
+}
+
+static void or_(bool canAssign) {
+    int elseJump = emitJump(OP_JUMP_IF_FALSE);
+    int endJump = emitJump(OP_JUMP);
+
+    patchJump(elseJump);
+    emitByte(OP_POP);
+
+    parsePrecedence(PREC_OR);
+    patchJump(endJump);
 }
 
 static void string(bool canAssign) {
@@ -384,7 +404,7 @@ ParseRule rules[] = {
     [TOKEN_IDENTIFIER]      = {variable, NULL,          PREC_NONE},
     [TOKEN_STRING]          = {string,   NULL,          PREC_NONE},
     [TOKEN_NUMBER]          = {number,   NULL,          PREC_NONE},
-    [TOKEN_AND]             = {NULL,     NULL,          PREC_NONE},
+    [TOKEN_AND]             = {NULL,     and_,          PREC_AND},
     [TOKEN_CLASS]           = {NULL,     NULL,          PREC_NONE},
     [TOKEN_ELSE]            = {NULL,     NULL,          PREC_NONE},
     [TOKEN_FALSE]           = {literal,  NULL,          PREC_NONE},
@@ -392,7 +412,7 @@ ParseRule rules[] = {
     [TOKEN_FUN]             = {NULL,     NULL,          PREC_NONE},
     [TOKEN_IF]              = {NULL,     NULL,          PREC_NONE},
     [TOKEN_NIL]             = {literal,  NULL,          PREC_NONE},
-    [TOKEN_OR]              = {NULL,     NULL,          PREC_NONE},
+    [TOKEN_OR]              = {NULL,     or_,           PREC_OR},
     [TOKEN_PRINT]           = {NULL,     NULL,          PREC_NONE},
     [TOKEN_RETURN]          = {NULL,     NULL,          PREC_NONE},
     [TOKEN_SUPER]           = {NULL,     NULL,          PREC_NONE},
