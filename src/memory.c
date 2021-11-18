0d1561b6b346f10a222d2d92cbe69a05949f9bf7
mbmcmullen27 <mbmcmullen27@gmail.com>
2021-11-18 08:06:35 -0600

    28.1-28.2

--- src/memory.c
+++ src/memory.c
@@ -73,9 +73,16 @@ static void blackenObject(Obj* object) {
 #endif
 
     switch(object->type) {
+        case OBJ_BOUND_METHOD: {
+            ObjBoundMethod* bound = (ObjBoundMethod*)object;
+            markValue(bound->receiver);
+            markObject((Obj*)bound->method);
+            break;
+        }
         case OBJ_CLASS: {
             ObjClass* klass = (ObjClass*)object;
             markObject((Obj*)klass->name);
+            markTable(&klass->methods);
             break;
         }
         case OBJ_CLOSURE: { 
@@ -113,7 +120,11 @@ static void freeObject(Obj* object) {
 #endif
 
     switch (object->type) {
+        case OBJ_BOUND_METHOD:
+            FREE(ObjBoundMethod, object);
         case OBJ_CLASS: {
+            ObjClass* klass = (ObjClass*)object;
+            freeTable(&klass->methods);
             FREE(ObjClass, object);
             break;
         }
